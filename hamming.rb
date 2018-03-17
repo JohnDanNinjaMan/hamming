4a006dfdfadad6a8e4fd80a179bacf0fa67139c5
Matthew Pickin <matthewpickin@aramis.home>
2018-03-17 19:58:41 +0000

    Basically made it more ruby. Used .each methods, and hopefully made it a bit more readable. unfortunately it's longer!

--- hamming.rb
+++ hamming.rb
@@ -3,13 +3,21 @@ module BookKeeping
   VERSION = 3
 end
 
+def same_size?(dna1,dna2)
+  raise ArgumentError.new if dna1.length!=dna2.length
+end
+
+def hamming_no(dna1,dna2)
+  hdistance=0
+  dna1.zip(dna2).each do |base1, base2|
+    if base1 != base2 then hdistance += 1 end
+  end
+  hdistance
+end
+
 class Hamming
   def self.compute(dna1,dna2)
-    raise ArgumentError.new if dna1.length!=dna2.length
-    hdistance=0
-    for i in 0..(dna1.length-1)
-      hdistance+=1 if dna1.chars[i]!=dna2.chars[i]
-    end
-    hdistance
+    same_size?(dna1,dna2)
+    hamming_no(dna1.split(''),dna2.split(''))
   end
 end
